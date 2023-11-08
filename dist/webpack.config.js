547fbd5110e37bcc051868be00790e8123cd16b2
Attila Oláh <attilaolah@gmail.com>
2023-11-08 20:05:43 +0100

    Add index.html.

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -4,29 +4,27 @@ module.exports = {
   entry: {
     style: [
       "normalize.css/normalize.css",
-    ]
+    ],
   },
   output: {
-    asyncChunks: false,
-    filename: "[name].js",
     module: true,
+    filename: "[name].js",
+    asyncChunks: false,
   },
   experiments: {
     futureDefaults: true,
     outputModule: true,
   },
   module: {
-    rules: [
-      {
-        test: /\.wasm$/,
-        type: "webassembly/async",
-      },
-    ],
+    rules: [{
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    }],
   },
   devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
+    static: [{
+      directory: __dirname,
+    }],
     compress: true,
     port: 8080,
   },
